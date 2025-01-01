6065422a38b703a496b3c1e1c8f149d3036ff89c
Divs-Git <divyansh.sri258@gmail.com>
2025-01-01 18:34:15 +0530

    created Route configuration

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import authController from '../controllers/auth.js';
+
+const router = express.Router();
+
+router.post('/login', authController.login);
+
+router.post('/register', authController.register);
+
+router.post('/send-otp', authController.sendOTP);
+
+router.post('/verify-otp', authController.verifyOTP);
+
+router.post('/forgot-password', authController.forgotPassword);
+
+router.patch('/reset-password', authController.resetPassword);
+
+export default router;
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import userController from '../controllers/user.js';
+import authController from '../controllers/auth.js';
+
+const router = express.Router();
+
+router.patch('/update-me', authController.protect, userController.updateMe);
